e3dff3e6df6b43768877ab42eb7523cd01b8eaaf
Jared Qin <jaredqin@gmail.com>
2020-10-29 21:47:20 +1100

    init by Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Crypto Currency Price Analytics
+      </header>
+    </div>
+  );
+}
+
+export default App;
